f833d9b4613609e6c0c0b47eeeb505e503dc90f7
sametd <samet.demir@ecmwf.int>
2025-03-04 21:56:45 +0000

    openid auth module

---
+++ pyaviso/authentication/openid_auth.py
@@ -0,0 +1,23 @@
+# (C) Copyright 1996- ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+class OpenidAuth:
+    """
+    OpenidAuth implements an OpenID authentication flow.
+    
+    It returns a Bearer header (using the shared secret from config.password) and adds
+    an extra header "X-Auth-Type" with the value "openid".
+    """
+    def __init__(self, config):
+        self.config = config
+
+    def header(self):
+        return {
+            "Authorization": f"Bearer {self.config.password}",
+            "X-Auth-Type": "openid"
+        }
